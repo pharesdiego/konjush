a696974c4d7eecbe65b5bd147e774e27c8b182bd
Diego Phares <pharesdiego@gmail.com>
2018-05-24 15:48:45 -0400

    create-react-app

--- index.js
+++
@@ -1,7 +0,0 @@
-const ReactDOM = require('react-dom'),
-			React = require('react'),
-			App = require('./app/react/app');
-
-
-require('./app/public/styles/main.scss');
-ReactDOM.render(<App />, document.getElementById('app'));
