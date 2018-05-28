e0e91f42905a484ad97f2b5a31c4a61910cb9762
Diego Phares <pharesdiego@gmail.com>
2018-05-28 09:40:26 -0400

    developing input

--- src/components/Conjugation/index.js
+++ src/components/Conjugation/index.js
@@ -1,22 +1,48 @@
 import React, { Component } from 'react';
 import './index.css';
 
-class InputVerb extends Component {
 
-  writingHandler = (event) => {
-    console.log(this.input.value)
+let input;
+const updateInputValue = value => input.value = value;
+
+const InputVerb = props => (
+  <input
+    placeholder='lets write'
+    onInput={ () => updateInputValue(input.value) }
+    onFocus={  }
+    onBlur={  }
+    ref={i => input = i}
+  />
+)
+
+const putLetterOnInputValue = letter => () => updateInputValue(input.value + letter);
+
+
+class InputVerbManager extends Component {
+  constructor(props){
+    super(props);
+    this.input = false;
   }
 
+  onInputHandler = () => {
+    
+  }
+
+
   render(){
-    return (
-      <input
-        placeholder='lets write'
-        onInput={ this.writingHandler }
-        ref={i => this.input = i}
-      />
+    return(
+      <div className='conjugation-input box-shape input-shape w-100 h-100 d-flex'>
+        <InputVerb 
+          onInputHandler = { onInputHandler }
+          onFocusHandler = { onFocusHandler }
+          onBlurHandler = { onBlurHandler }
+          inputElement = { this.input }
+        />
+      </div>
     )
   }
 }
+
 
 const Conjugation = props => (
   <div className='component'>
@@ -26,11 +52,32 @@ const Conjugation = props => (
            // aqui ira el aviso de informacion cargada
         }
         <div className='conjugation-sections component-body-section'>
+
           <div className='conjugation-input-container w-100'>
             <div className='conjugation-input box-shape input-shape w-100 h-100 d-flex'>
               <InputVerb />
             </div>
+
+            <div className='helpers-letter'>
+              {
+                ['ş', 'ğ', 'ı', 'ç', 'ü', 'ö']
+                  .map(letter => (
+                    <span
+                      onClick={ putLetterOnInputValue(letter) }
+                    > 
+                      { letter } 
+                    </span>
+                  ))
+              }
+            </div>
+
           </div>
+
+          <div className='verb-name w-100'>
+            <p>Gelmek</p>
+          </div>
+
+
         </div>
       </div>
     </div>
